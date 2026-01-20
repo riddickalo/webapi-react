f0f422f01ea309d0c241241b9d67e7985f148ee4
riddickalo <riddickxu333@gmail.com>
2026-01-21 01:44:46 +0800

    [feat] add a nc info

--- src/mock_server/data/nc.js
+++ src/mock_server/data/nc.js
@@ -9,7 +9,7 @@ export const mockNCStatus = [
     running_flag: 1,
     ncfile: "run_100.nc",
     maintainStatus: 0,
-    utilize_rate: 76
+    utilize_rate: 66
   },
   {
     nc_id: "NC-1-3",
@@ -21,17 +21,29 @@ export const mockNCStatus = [
     running_flag: 1,
     ncfile: "run_200.nc",
     maintainStatus: 0,
-    utilize_rate: 85
+    utilize_rate: 92
   },
   {
-    nc_id: "NC-2-1",
+    nc_id: "TT-1-14",
+    nc_ip: "192.168.1.14",
+    region: "Factory 1",
+    prod_line: "Blade-TB1",
+    station: "Ovening",
+    opStatus: "running",
+    running_flag: 1,
+    ncfile: "bb-200-80.g",
+    maintainStatus: 1,
+    utilize_rate: 78
+  },
+  {
+    nc_id: "EU-2-1",
     nc_ip: "192.168.2.1",
     region: "Factory 2",
     prod_line: "Gear Box-GB2",
     station: "Lathe turning",
     opStatus: "idle",
     running_flag: 1,
-    ncfile: "run_100.nc",
+    ncfile: "gb_l_0116.nc",
     maintainStatus: 0,
     utilize_rate: 33
   },
